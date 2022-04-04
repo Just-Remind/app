3bf1ee97cd65248dba379b22e295144ea2f93ac5
Loic Boset <>
2022-04-04 21:47:23 +0300

    Update /reminder endpoint with book notes

--- pages/api/reminder.ts
+++ pages/api/reminder.ts
@@ -1,16 +1,30 @@
+/* eslint-disable import/no-unresolved */
+/* eslint-disable import/extensions */
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 // import type { NextApiRequest, NextApiResponse } from 'next';
 // import schedule from 'node-schedule';
 import type { NextApiRequest, NextApiResponse } from 'next';
 import sgMail from '@sendgrid/mail';
+import prisma from '../../lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
+  const result = await prisma.note.findMany();
+  const indices = [];
+
+  while (indices.length < 5) {
+    const random = Math.floor(Math.random() * result.length);
+    indices.push(random);
+  }
+
+  const notes: string[] = [];
+  indices.forEach((index) => notes.push(result[index].content));
+
   sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
   const msg = {
     to: 'loic.boset@gmail.com', // Change to your recipient
     from: 'loic.boset@gmail.com', // Change to your verified sender
-    subject: 'Cheers to a new day!',
-    text: 'Hope your day has been well!',
+    subject: 'Remind 📚',
+    text: notes.join('\n '),
   };
 
   sgMail.send(msg);
