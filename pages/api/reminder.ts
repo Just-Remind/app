cd05ad17aa397517ff0313935d485a892fe5a16f
Loic Boset <>
2022-04-05 21:14:49 +0300

    Add email html body

--- pages/api/reminder.ts
+++ pages/api/reminder.ts
@@ -8,7 +8,16 @@ import sgMail from '@sendgrid/mail';
 import prisma from '../../lib/prisma';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const result = await prisma.note.findMany();
+  const result = await prisma.note.findMany({
+    select: {
+      content: true,
+      book: {
+        select: {
+          title: true,
+        },
+      },
+    },
+  });
   const indices = [];
 
   while (indices.length < 5) {
@@ -17,14 +26,32 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   const notes: string[] = [];
-  indices.forEach((index) => notes.push(result[index].content));
+  indices.forEach((index) => notes.push(result[index]));
 
   sgMail.setApiKey(process.env.SENDGRID_API_KEY || '');
   const msg = {
     to: 'loic.boset@gmail.com', // Change to your recipient
     from: 'loic.boset@gmail.com', // Change to your verified sender
     subject: 'Remind 📚',
-    text: notes.join('\n '),
+    text: notes.map((note) => note.content).join('\n '),
+    html: `<h3>Remind</h3>
+
+      <h4>${notes[0].book.title}</h4>
+      <p>${notes[0].content}</p>
+
+      <h4>${notes[1].book.title}</h4>
+      <p>${notes[1].content}</p>
+
+      <h4>${notes[2].book.title}</h4>
+      <p>${notes[2].content}</p>
+
+      <h4>${notes[3].book.title}</h4>
+      <p>${notes[3].content}</p>
+
+      <h4>${notes[4].book.title}</h4>
+      <p>${notes[4].content}</p>
+
+      <p>Stay curious!</p> `,
   };
 
   sgMail
