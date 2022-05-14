4a1ecc93fad9a7d540ac495af9daf9a6e79446a4
Loic Boset <>
2022-05-14 17:51:48 +0300

    Update add_book query

--- pages/api/add_book.ts
+++ pages/api/add_book.ts
@@ -29,139 +29,50 @@ const handler = async (
       },
     },
   });
+
   if (!user) return res.status(500).json("user not found");
 
-  // return res.status(200).json(user);
-
-  const promises: Promise<number | void>[] = [];
-  const existingBooks = [];
-
-  importedBooks.map((importedBook) => {
-    const promise = prisma.book
-      .findFirst({
-        where: {
-          userId: user.id,
-          title: importedBook.title,
-        },
-        select: {
-          id: true,
-          title: true,
-          notes: {
-            select: {
-              content: true,
+  await prisma.user.update({
+    where: {
+      email: body.user.email,
+    },
+    data: {
+      books: {
+        upsert: importedBooks.map((book) => ({
+          where: {
+            userId_title: {
+              userId: user.id,
+              title: book.title,
             },
           },
-        },
-      })
-      .then((response) => existingBooks.push(response))
-      .catch((error) => res.status(500).json(error));
-
-    promises.push(promise);
+          update: {
+            notes: {
+              createMany: {
+                skipDuplicates: true,
+                data: book.highlights.map((highlight) => ({
+                  content: highlight,
+                })),
+              },
+            },
+          },
+          create: {
+            title: book.title,
+            author: book.author,
+            notes: {
+              createMany: {
+                skipDuplicates: true,
+                data: book.highlights.map((highlight) => ({
+                  content: highlight,
+                })),
+              },
+            },
+          },
+        })),
+      },
+    },
   });
 
-  await Promise.all(promises)
-    .then(() => {
-      console.log("BOOKS", existingBooks.length);
-      res.status(200).json("success");
-    })
-    .catch((error) => res.status(500).json(error));
-
-  // console.log("USER BOOKS v2", user.books.length);
-
-  // const createdHighlights: string[] = [];
-  // const createdBooks: string[] = [];
-
-  // console.log("START");
-  // await Promise.all(
-  //   importedBooks.map(
-  //     async (importedBook) =>
-  //       prisma.book.findFirst({
-  //         where: {
-  //           userId: user.id,
-  //           title: importedBook.title,
-  //         },
-  //         select: {
-  //           id: true,
-  //           title: true,
-  //           notes: {
-  //             select: {
-  //               content: true,
-  //             },
-  //           },
-  //         },
-  //       })
-
-  //     // existingBooks.push(existingBook);
-  //   )
-  // )
-  //   .then((response) => {
-  //     console.log("RESPONSE", response);
-  //     res.status(200);
-  //   })
-  //   .catch(() => res.status(500));
-
-  // console.log("existingBooks", existingBooks.length);
-
-  // try {
-  //   console.log("HOLLA?");
-  //   importedBooks.map(async (importedBook) => {
-  //     const existingBook = await prisma.book.findFirst({
-  //       where: {
-  //         userId: user.id,
-  //         title: importedBook.title,
-  //       },
-  //       select: {
-  //         id: true,
-  //         title: true,
-  //         notes: {
-  //           select: {
-  //             content: true,
-  //           },
-  //         },
-  //       },
-  //     });
-  //     console.log("HELLO?", existingBook ? existingBook.title : "no book");
-  //     if (existingBook) {
-  //       const bookHighlights = existingBook.notes.map((note) => note.content);
-
-  //       importedBook.highlights.map(async (highlight) => {
-  //         if (!bookHighlights.includes(highlight)) {
-  //           await prisma.note.create({
-  //             data: {
-  //               content: highlight,
-  //               bookId: existingBook.id,
-  //             },
-  //           });
-
-  //           createdHighlights.push(highlight);
-  //         }
-  //       });
-  //     } else {
-  //       const formatedNotes = importedBook.highlights.map((highlight) => ({
-  //         content: highlight,
-  //       }));
-  //       await prisma.book.create({
-  //         data: {
-  //           userId: user.id,
-  //           title: importedBook.title,
-  //           author: importedBook.author,
-  //           notes: {
-  //             createMany: {
-  //               data: formatedNotes,
-  //             },
-  //           },
-  //         },
-  //       });
-
-  //       createdBooks.push(importedBook.title);
-  //     }
-  //   });
-  //   console.log("SUCCESS", importedBooks.length);
-  //   res.status(200).json({ createdBooks, createdHighlights });
-  // } catch (error) {
-  //   console.log("ERROR", error);
-  //   res.status(500).json(error);
-  // }
+  return res.status(200).json("SUCCESS");
 };
 
 export default handler;
