75346a7dbcbbc340519b2f463454c17a1d3a6a43
Loic Boset <loic.boset@gmail.com>
2022-06-12 15:25:51 +0100

    Update add_book api

--- pages/api/add_book.ts
+++ pages/api/add_book.ts
@@ -17,7 +17,7 @@ const handler = async (
 
   res.setHeader(
     "Access-Control-Allow-Origin",
-    `chrome-extension://iidldfielonfgiabbjjkbigjjclcpefa`
+    `chrome-extension://${process.env.NEXT_PUBLIC_CHROME_EXTENSION_ID}`
   );
 
   if (!body.user.email) return res.status(500).json("no user found");
