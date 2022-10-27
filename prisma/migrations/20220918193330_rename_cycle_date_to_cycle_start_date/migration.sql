d9efb2ae00d4ccffcf950eb27682f29518c461a4
Loic Boset <loic.boset@gmail.com>
2022-10-27 20:51:14 +0200

    update migrations

--- prisma/migrations/20220918193330_rename_cycle_date_to_cycle_start_date/migration.sql
+++ prisma/migrations/20220918193330_rename_cycle_date_to_cycle_start_date/migration.sql
@@ -1,7 +1,7 @@
--- -- AlterTable
-ALTER TABLE "CronJob" ADD COLUMN     "cycleDate" TIMESTAMP(3);
+-- -- -- AlterTable
+-- ALTER TABLE "CronJob" ADD COLUMN     "cycleDate" TIMESTAMP(3);
 
--- -- AlterTable
-ALTER TABLE "Highlight" ADD COLUMN     "lastSentOn" TIMESTAMP(3);
+-- -- -- AlterTable
+-- ALTER TABLE "Highlight" ADD COLUMN     "lastSentOn" TIMESTAMP(3);
 
 ALTER TABLE "CronJob" RENAME COLUMN "cycleDate" TO "cycleStartDate"
